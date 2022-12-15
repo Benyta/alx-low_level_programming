f31e64f153bfd1395d5620ed5330c297a7fbe4f0
Benyta <kelechibenita@yahoo.comgit config --global user.email kelechibenita@yahoo.com>
2022-12-14 16:58:40 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0
  */
 
@@ -11,7 +11,7 @@ int main(void)
 
 	_putchar('_');
 	_putchar('p');
- 	_putchar('u');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
