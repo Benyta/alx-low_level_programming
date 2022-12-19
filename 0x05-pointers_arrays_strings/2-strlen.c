275bb7536d40433505580e45dbb2fa20245f8ef0
Benyta <kelechibenita@yahoo.comgit config --global user.email kelechibenita@yahoo.com>
2022-12-19 13:35:10 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,8 @@
-#
-
-include "main.h"
-
-
-
+#include "main.h"
 
 /**
  * _strlen-Finds the length of a string.
+ *
  * @s:String pointer to the string whose length is to be found.
  *
  * Return: returns the length of the string.
@@ -14,11 +10,9 @@ include "main.h"
 
 int _strlen(char *s)
 {
-int p = 0;
-/*incremeant up to when the last character is NULL,\0*/
-	
-while (*(s + p) != 0)
-{
-p++;
+	int index;
+
+	for (index = 0; s[index] != '\0'' ; index++)
+
+	return (index);
 }
-return (p);
